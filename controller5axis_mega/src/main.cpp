0b7370710dddb732f6c50f1ffb42b97eef5a9670
mihailnik <80267250+mihailnik@users.noreply.github.com>
2022-02-01 23:20:39 +0200

    created

---
+++ controller5axis_mega/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+// 5ти канальный пульт управления сплайнером
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
